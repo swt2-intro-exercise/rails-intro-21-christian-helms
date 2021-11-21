777c83fe29fd4606be47cb9163f8a62e9c2f3f61
christian-helms <christian.z.helms@gmail.com>
2021-11-22 00:10:34 +0100

    implements issue #4

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_220335) do
+ActiveRecord::Schema.define(version: 2021_11_21_225049) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "homepage"
+    t.string "last_name"
   end
 
 end
