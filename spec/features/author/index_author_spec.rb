6e8f8c25fd35d530e1151455a6a1859b2c357756
christian-helms <christian.z.helms@gmail.com>
2021-11-22 12:02:03 +0100

    failing test for issue #8

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 describe "Index author page", type: :feature do
 	it "should display the the full names and homepage links/
-		of all authors in a table" do
+		of all authors in a table and link to their details pages" do
 		@alan = FactoryBot.create :author
 		visit "authors#index"
 		expect(page).to have_text(@alan.name())
 		expect(page).to have_link(@alan.homepage, href: @alan.homepage)
+		expect(page).to have_link(href: "/authors/" + (@alan.id).to_s())
 	end
 	it "should also contain a link to add a new author" do
 		visit "authors#index"
