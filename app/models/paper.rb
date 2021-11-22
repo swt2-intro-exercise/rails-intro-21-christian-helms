3f3cbcf2f5169fb57cd24b9940c6052d9a4cd983
christian-helms <christian.z.helms@gmail.com>
2021-11-22 17:57:19 +0100

    implements issue #18

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,7 +1,9 @@
 class Paper < ApplicationRecord
+	has_and_belongs_to_many :authors
 
 	validates :title, presence: true
 	validates :venue, presence: true
 	validates :year, presence: true
 	validates :year, numericality: {only_integer: true}
+
 end
