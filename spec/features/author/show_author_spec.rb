533344bfc6577f1fc03ff2d309c07713077cb650
christian-helms <christian.z.helms@gmail.com>
2021-11-22 03:17:07 +0100

    implements issue #6 (and fixes the corresponding test)

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -6,6 +6,6 @@ describe "Show author page", type: :feature do
 		visit author_path(@alan)
 		expect(page).to have_text(@alan.first_name)
 		expect(page).to have_text(@alan.last_name)
-		expect(page).to have_text(@alan.home_page)
+		expect(page).to have_text(@alan.homepage)
 	end
 end
