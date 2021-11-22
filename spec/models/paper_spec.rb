cf7ccb1f56f9b5009b44137bead580b7f956214e
christian-helms <christian.z.helms@gmail.com>
2021-11-22 15:50:00 +0100

    failing test for issue #15

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not be valid if it has no title" do
+    paper = Paper.new(venue: "mind 49: 433-460", year: 1950)
+    expect(paper).to_not be_valid
+  end
 end
