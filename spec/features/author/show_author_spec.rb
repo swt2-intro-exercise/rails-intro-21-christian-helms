a6b04f561a49cf9473d8b7b7a81f5aa071a40321
christian-helms <christian.z.helms@gmail.com>
2021-11-22 13:57:36 +0100

    failing test and implementation of issue #12

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -8,4 +8,9 @@ describe "Show author page", type: :feature do
 		expect(page).to have_text(@alan.last_name)
 		expect(page).to have_text(@alan.homepage)
 	end
+	it "should have a link to the edit page of the author" do
+		@alan = FactoryBot.create :author
+		visit author_path(@alan)
+		expect(page).to have_link(href: edit_author_path(@alan))
+	end
 end
